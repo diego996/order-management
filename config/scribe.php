db863861ec3f27a0b67b2fef1e63db2888c24501
diego_selexi <diego_barzaghi@selexi.it>
2025-05-07 18:09:12 +0200

    fix: Update base URL in Scribe configuration and add missing newline in DatabaseSeeder

--- config/scribe.php
+++ config/scribe.php
@@ -16,7 +16,7 @@ return [
 
     // The base URL displayed in the docs.
     // If you're using `laravel` type, you can set this to a dynamic string, like '{{ config("app.tenant_url") }}' to get a dynamic base URL.
-    'base_url' => config("app.url"),
+    'base_url' => "https://order-management-master-er9oqj.laravel.cloud/",
 
     // Routes to include in the docs
     'routes' => [
